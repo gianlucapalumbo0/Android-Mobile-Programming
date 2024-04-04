26a9a398c378621164c34179924fbb4c868a8a97
gianlucapalumbo0 <g.palumbo40@studenti.unisa.it>
2024-04-04 08:20:05 +0200

    Frammenti Statico

---
+++ FrammentiStatico/app/src/main/java/com/example/frammentistatico/FragmentA.java
@@ -0,0 +1,69 @@
+package com.example.frammentistatico;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import kotlin.OverloadResolutionByLambdaReturnType;
+
+public class FragmentA extends Fragment {
+
+    public FragmentA(){
+        //richiesto costruttore pubblico
+    }
+
+    @Override
+    public void onAttach(Activity activity){
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_a, container, false);
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle s){
+        super.onActivityCreated(s);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroyView(){
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach(){
+        super.onDetach();
+    }
+}
---
+++ FrammentiStatico/app/src/main/java/com/example/frammentistatico/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.frammentistatico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
+
+
+}
